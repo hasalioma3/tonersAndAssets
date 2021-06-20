c3512ed27ce83c18550c6ac8663eaca03ab24d01
hassan omar <hassan.omar@naivas.co.ke>
2021-06-21 00:49:27 +0300

    Added remove Logic

--- assets/models.py
+++ assets/models.py
@@ -3,15 +3,18 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-
 class Make(models.Model):
     name = models.CharField(max_length=255, blank=True, null=True,)
 
     def __str__(self):
         return self.name
+
+
 class Amodel(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
-    make = models.ForeignKey(Make, related_name='Model',null=True, blank=True,on_delete=models.CASCADE)
+    make = models.ForeignKey(Make, related_name='Model',
+                             null=True, blank=True, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
 
@@ -24,10 +27,14 @@ class Location(models.Model):
 
 
 class Staff(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    location = models.ForeignKey('Location', on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    location = models.ForeignKey(
+        'Location', on_delete=models.CASCADE, null=True, blank=True)
+
     def __str__(self):
         return str(self.user)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
@@ -35,58 +42,72 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Logo(models.Model):
-    logo=models.ImageField(upload_to="static/img/logo")
-    
+    logo = models.ImageField(upload_to="static/img/logo")
+
+
 class Asset(models.Model):
-    make = models.ForeignKey(Make, on_delete=models.CASCADE, null=True, blank=True)
-    amodel = models.ForeignKey(Amodel, on_delete=models.CASCADE, null=True, blank=True, related_name='model')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=200, null=True,blank=True)
+    make = models.ForeignKey(
+        Make, on_delete=models.CASCADE, null=True, blank=True)
+    amodel = models.ForeignKey(
+        Amodel, on_delete=models.CASCADE, null=True, blank=True, related_name='model')
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
     barcode = models.CharField(max_length=200, null=True, blank=True,)
     serialNumber = models.CharField(max_length=200, null=True, blank=True)
     price = models.FloatField()
     lpoNumber = models.CharField(max_length=200, null=True, blank=True)
-    accessory= models.BooleanField(default=False)
-    location=models.ForeignKey(Location,on_delete=models.CASCADE, null=True, blank=True)
-    transit=models.BooleanField(default=False)
-    
+    accessory = models.BooleanField(default=False)
+    location = models.ForeignKey(
+        Location, on_delete=models.CASCADE, null=True, blank=True)
+    transit = models.BooleanField(default=False)
+
     def __str__(self):
-        return self.name 
+        return self.name
+
 
 class Delivery(models.Model):
-    staff = models.ForeignKey(Staff, on_delete=models.CASCADE,blank=True,null=True)
+    staff = models.ForeignKey(
+        Staff, on_delete=models.CASCADE, blank=True, null=True)
     date_dispatched = models.DateTimeField(auto_now_add=True)
-    dispatched= models.BooleanField(default=False)
-    deliveryNo=models.CharField(max_length=200, null=True, blank=True)
-    fromLocation= models.ForeignKey(Location, on_delete=models.CASCADE, null=True, blank=True)
-    toLocation= models.ForeignKey(Location, on_delete=models.CASCADE, null=True, blank=True, related_name='ToLocation')
+    dispatched = models.BooleanField(default=False)
+    deliveryNo = models.CharField(max_length=200, null=True, blank=True)
+    fromLocation = models.ForeignKey(
+        Location, on_delete=models.CASCADE, null=True, blank=True)
+    toLocation = models.ForeignKey(
+        Location, on_delete=models.CASCADE, null=True, blank=True, related_name='ToLocation')
 
     def __str__(self):
         return self.deliveryNo
-    
+
     @property
     def key1(self):
         return str(self.id).zfill(6)
- 
+
     @property
     def get_delivery_items_no(self):
-        deliveryassets= self.deliveryasset_set.all()
+        deliveryassets = self.deliveryasset_set.all()
         total = sum([asset.quantity for asset in deliveryassets])
         return total
+
     def get_delivery_assets(self):
-        deliveryassets= self.deliveryasset_set.all()
+        deliveryassets = self.deliveryasset_set.all()
         return deliveryassets
+
+
 class DeliveryAsset(models.Model):
-    asset = models.ForeignKey(Asset, on_delete=models.CASCADE, null=True, blank=True)
-    delivery= models.ForeignKey(Delivery, on_delete=models.CASCADE, null=True, blank=True)
-    quantity = models.IntegerField(default=0,null=True, blank=True)
+    asset = models.ForeignKey(
+        Asset, on_delete=models.CASCADE, null=True, blank=True)
+    delivery = models.ForeignKey(
+        Delivery, on_delete=models.CASCADE, null=True, blank=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
     date_dispatched = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.asset)
 
     def get_deliveryNoteNo(self):
-        dnn=self.delivery_set.all()
+        dnn = self.delivery_set.all()
         return dnn
-
